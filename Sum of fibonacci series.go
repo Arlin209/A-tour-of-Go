760804f53cd2f22c95d35781a03774cee1e3fe1e
Arlin209 <arlinwilson18@gmail.com>
2026-07-16 08:25:47 +0530

    Sum of Fibonacci series

---
+++ Sum of fibonacci series.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func fibonacci(n int) int {
+	a, b := 0, 1
+	for i := 0; i < n; i++ {
+		a, b = b, a+b
+	}
+	return a
+}
+
+func main() {
+	fmt.Println(fibonacci(9)) // 34
+}
